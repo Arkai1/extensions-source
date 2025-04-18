82866aae249565e6b40cddf0c6fd15c2f162199c
Arkai1 <codinsanemobile@gmail.com>
2025-04-18 13:02:32 +0530

    Update Kaido.kt

--- src/en/kaido/src/eu/kanade/tachiyomi/animeextension/en/kaido/Kaido.kt
+++ src/en/kaido/src/eu/kanade/tachiyomi/animeextension/en/kaido/Kaido.kt
@@ -1,13 +1,9 @@
 package eu.kanade.tachiyomi.animeextension.en.kaido
 
-import eu.kanade.tachiyomi.animesource.model.SAnime
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.lib.megacloudextractor.MegaCloudExtractor
 import eu.kanade.tachiyomi.lib.streamtapeextractor.StreamTapeExtractor
 import eu.kanade.tachiyomi.multisrc.zorotheme.ZoroTheme
-import eu.kanade.tachiyomi.network.GET
-import okhttp3.Request
-import org.jsoup.nodes.Element
 
 class Kaido : ZoroTheme(
     "en",
@@ -22,17 +18,9 @@ class Kaido : ZoroTheme(
     override val id = 5569316556494057205L
 
     override val ajaxRoute = "/v2"
-    
+
     private val streamtapeExtractor by lazy { StreamTapeExtractor(client) }
     private val megaCloudExtractor by lazy { MegaCloudExtractor(client, headers, preferences) }
-
-    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/recently-updated?page=$page", docHeaders)
-
-    override fun popularAnimeFromElement(element: Element): SAnime {
-        return super.popularAnimeFromElement(element).apply {
-            url = url.substringBefore("?")
-        }
-    }
 
     override fun extractVideo(server: VideoData): List<Video> {
         return when (server.name) {
